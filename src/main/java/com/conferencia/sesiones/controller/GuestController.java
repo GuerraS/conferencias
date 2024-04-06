86c049f8c0b7d5c60102460a92bbe844268f84c6
Edie <eduardoos@outlook.com>
2024-04-06 02:09:48 -0600

    Feat: se modifica controlador Guest

--- src/main/java/com/conferencia/sesiones/controller/GuestController.java
+++ src/main/java/com/conferencia/sesiones/controller/GuestController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.conferencia.sesiones.entity.Guest;
 import com.conferencia.sesiones.service.GuestService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name="Guest Controller", description="Controlador para crud de invitados")
 @RestController
 @RequestMapping(value = "/invitados")
 public class GuestController {
@@ -21,7 +24,7 @@ public class GuestController {
 	private GuestService guestService;
 	
 	@GetMapping
-	public ResponseEntity<List<Guest>> ListProducts(@RequestParam(name="categoryId", required = false) Long categoryId){
+	public ResponseEntity<List<Guest>> ListGuests(@RequestParam(name="categoryId", required = false) Long categoryId){
 		List<Guest> listGuest = new ArrayList<Guest>();
 		
 		listGuest = guestService.listAllGuest();
